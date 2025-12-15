1dd92e5c333a187d2d05cb509a0f38ccc3de215e
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-12-15 14:17:41 +0600

    Remove some unnecessary code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async'
+// import { Helmet } from 'react-helmet-async'
 import { motion } from 'framer-motion'
 import { ChefHat, Clock, Shield, Truck } from 'lucide-react'
 import HeroSection from '../components/HeroSection'
